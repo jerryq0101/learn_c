7e69c692bdc918c44eb13e1ddc6b572f8058b3d6
Jerry Qi <jerryq0101@gmail.com>
2024-10-21 23:08:49 -0400

    ex5-9

---
+++ 5.9/ex5-9.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/*
+Exercise 5-9. Rewrite the routines day_of_year and month_day with pointers instead of
+indexing.
+*/
+
+static char non_leap[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+static char leap[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+static char *daytab[2] = {non_leap, leap};
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int yearday, int *pmonth, int *pday);
+
+int main(void) {
+        printf("%d\n", day_of_year(2024, 10, 21));
+
+        int a = 0;
+        int b = 0;
+
+        int *month = &a;
+        int *day = &b;
+        month_day(2023, 294, month, day);
+        
+        printf("%d %d\n", a, b);
+}
+
+/* day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day)
+{
+        int i, leap;
+        leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        for (i = 1; i < month; i++)
+                day += (*daytab+leap)[i];
+        return day;
+}
+
+/* month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+        int i, leap;
+        leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        for (i = 1; yearday > (*daytab+leap)[i]; i++)
+                yearday -= (*daytab+leap)[i];
+        *pmonth = i;
+        *pday = yearday;
+}
