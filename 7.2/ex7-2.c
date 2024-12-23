3cec06f7093c83f9cf565a1569231f997c79bd14
Jerry Qi <jerryq0101@gmail.com>
2024-12-22 16:57:07 -0800

    ex7-2

---
+++ 7.2/ex7-2.c
@@ -0,0 +1,34 @@
+/*
+Exercise 7-2. Write a program that will print arbitrary input in a sensible way. As a
+minimum, it should print non-graphic characters in octal or hexadecimal according to local
+custom, and break long text lines. 
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    int count;
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (!isgraph(c))
+        {
+            // c automatically has a decimal value,
+            // printf prints decimal as a hex. 
+            printf("%02x", c);
+        }
+        else
+        {
+            putchar(c);
+        }
+        
+        count++;
+
+        if (count == 10)
+        {
+            printf("\n");
+        }
+    }
+}
