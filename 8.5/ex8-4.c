7dc3e88309c10e5588c4840ac2d32e3e0febf891
Jerry Qi <jerryq0101@gmail.com>
2024-12-30 17:10:21 -0800

    ex8-4 initial

---
+++ 8.5/ex8-4.c
@@ -0,0 +1,216 @@
+/*
+Exercise 8-4. The standard library function
+int fseek(FILE *   fp, long offset, int origin)
+is identical to lseek except that fp is a file pointer instead of a file descriptor and return value
+is an int status, not a position. Write fseek. Make sure that your fseek coordinates properly
+with the buffering done for the other functions of the library. 
+*/
+
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <string.h>
+
+#define EOF (-1)
+#define BUFSIZ 1024
+#define OPEN_MAX 20 /* max #files open at once */
+
+typedef struct _iobuf
+{
+    int cnt;    /* characters left */
+    char *ptr;  /* next character position */
+    char *base; /* location of buffer */
+    int flag;   /* mode of file access */
+    int fd;     /* file descriptor */
+} FILE;
+
+enum _flags
+{
+    _READ = 01,  /* file open for reading */
+    _WRITE = 02, /* file open for writing */
+    _UNBUF = 04, /* file is unbuffered */
+    _EOF = 010,  /* EOF has occurred on this file */
+    _ERR = 020   /* error occurred on this file */
+};
+
+
+extern FILE _iob[OPEN_MAX] = { /* stdin, stdout, stderr, and the rest are empty slots*/
+ { 0, (char *) 0, (char *) 0, _READ, 0 },
+ { 0, (char *) 0, (char *) 0, _WRITE, 1 },
+ { 0, (char *) 0, (char *) 0, _WRITE | _UNBUF, 2 }
+};
+#define stdin (&_iob[0])
+#define stdout (&_iob[1])
+#define stderr (&_iob[2])
+
+
+int _fillbuf(FILE *);
+int _flushbuf(int, FILE *);
+
+#define feof(p) ((p)->flag & _EOF) != 0)
+#define ferror(p) ((p)->flag & _ERR) != 0)
+#define fileno(p) ((p)->fd)
+#define getc(p) (--(p)->cnt >= 0                  \
+                     ? (unsigned char)*(p)->ptr++ \
+                     : _fillbuf(p))
+
+#define getchar() getc(stdin)
+#define putcher(x) putc((x), stdout)
+
+#define PERMS 0666 /* RW for owner, group, others */
+#define MAXLINE 20
+
+int fseek(FILE* fp, long offset, int origin);
+
+
+FILE *fopen(char *name, char *mode)
+{
+    int fd;
+    FILE *fp;
+    if (*mode != 'r' && *mode != 'w' && *mode != 'a')
+        return NULL;
+    for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
+        if ((fp->flag & (_READ | _WRITE)) == 0)
+            break;             /* found free slot */
+    if (fp >= _iob + OPEN_MAX) /* no free slots */
+        return NULL;
+    if (*mode == 'w')
+        fd = creat(name, PERMS);
+    else if (*mode == 'a')
+    {
+        if ((fd = open(name, O_WRONLY, 0)) == -1)
+            fd = creat(name, PERMS);
+        lseek(fd, 0L, 2);
+    }
+    else
+        fd = open(name, O_RDONLY, 0);
+    if (fd == -1) /* couldn't access name */
+        return NULL;
+    fp->fd = fd;
+    fp->cnt = 0;
+    fp->base = NULL;
+    fp->flag = (*mode == 'r') ? _READ : _WRITE;
+    return fp;
+}
+
+
+int _flushbuf(int c, FILE* fp) 
+{
+    if (fp->base == NULL)               // base hasn't been allocated
+    {
+        fp->base = malloc(MAXLINE);     // Allocate buffer for base
+        fp->ptr = fp->base;
+    }
+
+    // Write HANDLES: buffer has characters or buffer doesn't have anything.
+    int bytes_written = write(fp->fd, fp->base, fp->ptr - fp->base);
+
+    // We've written the buffer contents to the file
+    /*
+    Understanding:
+    
+    On the outside, we need the character passed in.
+    On the inside, we need to put this new character in the buffer and make sure that the file will get impacted by this character
+    
+    Since the write expression above controls the number of characters we print to the file (if early flush),
+    we won't overwrite any (original) file contents with a non full buffer. 
+    - THEREFORE, we don't need read.
+    
+    WE WRITE ONLY the non null characters in the buffer.
+    
+    No READ needed.
+
+    Reset ptr to base. Purpose of ptr: point to next position in buffer to change, also showing which characters in buffer are modified.
+    Reset count. Purpose: to tell us buffer size
+    Decrement count by 1. Since we modified one thing in buffer already.
+    */
+
+    free(fp->base);                     // clear previous contents
+    fp->base = malloc(MAXLINE);         // reset values
+
+    fp->ptr = fp->base;
+    fp->cnt = MAXLINE;
+    *(fp->ptr) = c;
+    (fp->ptr)++;
+    fp->cnt--;
+
+    // put the character at the initial position of the new buffer
+    return c;
+}
+
+
+int fflush(FILE* stream)
+{
+    if (stream == NULL)
+    {
+        return -1;
+    }
+
+    if (stream->base == NULL || stream->ptr == NULL)   // Check if buffer base hasn't been allocated
+    {
+        stream->base = malloc(MAXLINE);
+        stream->ptr = stream->base;
+    }
+
+    int bytes_written = write(stream->fd, stream->base, stream->ptr - stream->base);        // writes buffer to fd
+
+    free(stream->base);                 // deallocate previous values
+    stream->base = malloc(MAXLINE);     // reset buffer values
+    
+    stream->ptr = stream->base;     // set pointer to the first element
+    stream->cnt = MAXLINE;          // reset count to initial
+    
+    return 0;
+}
+
+
+int fclose(FILE* stream)
+{
+    if ((stream->flag & _WRITE) == _WRITE)      // if we are writing, flush output to file
+    {
+        fflush(stream);
+    }
+    
+    if (stream->base)                           // Free buffer.
+    {
+        free(stream->base);
+    }
+    
+    return close(stream->fd) ? EOF : 0;
+}
+
+
+int fseek(FILE* fp, long offset, int origin)
+{
+	if (((fp->flag) & _WRITE) == _WRITE)	// write op file
+	{
+		// flush potential changes before moving file position
+		fflush(fp);
+	}
+	else if ((fp->flag & _READ) == _READ)	// read op file
+	{
+		// invalidate the buffer and positions (as we are at a new position)
+		fp->ptr = fp->base;
+		fp->cnt = -1;
+	}
+	
+	int pos = lseek(fp->fd, offset, origin);
+	if (pos == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+
+int main(void)
+{
+	FILE* in = fopen("in3.txt", "r");
+	fseek(in, 4, 0);
+}
