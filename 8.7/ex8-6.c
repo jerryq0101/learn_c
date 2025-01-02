a2c50d77c9954444919d626555802564284cf82f
Your Name <you@example.com>
2025-01-01 17:46:07 -0800

    ex8-6 progress

--- 8.7/ex8-6.c
+++ 8.7/ex8-6.c
@@ -23,11 +23,53 @@ typedef union header Header;
 static Header base;          /* empty list to get started */
 static Header *freep = NULL; /* start of free list */
 
+/* free: put block ap in free list */
+void free(void *ap)
+{
+    Header *bp, *p;
+    bp = (Header *)ap - 1; /* point to block header */
+    for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+            break; /* freed block at start or end of arena */
+    if (bp + bp->s.size == p->s.ptr)
+    { /* join to upper nbr */
+        bp->s.size += p->s.ptr->s.size;
+        bp->s.ptr = p->s.ptr->s.ptr;
+    }
+    else
+        bp->s.ptr = p->s.ptr;
+    if (p + p->s.size == bp)
+    { /* join to lower nbr */
+        p->s.size += bp->s.size;
+        p->s.ptr = bp->s.ptr;
+    }
+    else
+        p->s.ptr = bp;
+    freep = p;
+}
+
+#define NALLOC 1024 /* minimum #units to request */
+/* morecore: ask system for more memory */
+static Header *morecore(unsigned nu)
+{
+    char *cp, *sbrk(int);
+    Header *up;
+    if (nu < NALLOC)
+        nu = NALLOC;
+    cp = sbrk(nu * sizeof(Header));
+
+    if (cp == (char *)-1) /* no space at all */
+        return NULL;
+    up = (Header *)cp;
+    up->s.size = nu;
+    free((void *)(up + 1));
+    return freep;
+}
+
 /* malloc: general-purpose storage allocator */
 void *malloc(unsigned nbytes)
 {
     Header *p, *prevp;
-    Header *moreroce(unsigned);
     unsigned nunits;
     nunits = (nbytes + sizeof(Header) - 1) / sizeof(union header) + 1;
     if ((prevp = freep) == NULL)
@@ -56,62 +98,25 @@ void *malloc(unsigned nbytes)
     }
 }
 
+
 void* calloc(size_t nitems, size_t size)
 {
-    void* store_arr[nitems * size];
-    for (int i = 0; i < nitems; i++)
+    void* ptr;
+    if ((ptr = malloc(nitems * size)) == NULL)
     {
-        store_arr[i] = malloc(size);
+        return NULL;
     }
-    return store_arr;
+    
+    for (int i = 0; i < nitems*size; i+=size)
+    {
+        // do a memset on i+ptr to set to 0
+    }
 }
-
 
 int main(void)
 {
+    void** arr = calloc(5, 5);
     
 }
 
-#define NALLOC 1024 /* minimum #units to request */
-/* morecore: ask system for more memory */
-static Header *morecore(unsigned nu)
-{
-    char *cp, *sbrk(int);
-    Header *up;
-    if (nu < NALLOC)
-        nu = NALLOC;
-    cp = sbrk(nu * sizeof(Header));
-
-    if (cp == (char *)-1) /* no space at all */
-        return NULL;
-    up = (Header *)cp;
-    up->s.size = nu;
-    free((void *)(up + 1));
-    return freep;
-}
-
-/* free: put block ap in free list */
-void free(void *ap)
-{
-    Header *bp, *p;
-    bp = (Header *)ap - 1; /* point to block header */
-    for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
-        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-            break; /* freed block at start or end of arena */
-    if (bp + bp->s.size == p->s.ptr)
-    { /* join to upper nbr */
-        bp->s.size += p->s.ptr->s.size;
-        bp->s.ptr = p->s.ptr->s.ptr;
-    }
-    else
-        bp->s.ptr = p->s.ptr;
-    if (p + p->s.size == bp)
-    { /* join to lower nbr */
-        p->s.size += bp->s.size;
-        p->s.ptr = bp->s.ptr;
-    }
-    else
-        p->s.ptr = bp;
-    freep = p;
-}
 
